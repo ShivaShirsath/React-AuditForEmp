c5e675dc24bb8c8f435054e2522842f4d7f69691
Imran Fattekhan <imran.fattekhan@p99soft.com>
2023-08-18 18:18:34 +0530

    + Changes Visualizer, + ModificationVisualizer.

--- src/components/AuditLog.jsx
+++ src/components/AuditLog.jsx
@@ -22,13 +22,12 @@ const AuditLog = () => {
     return `${istDateTime.getUTCFullYear()}-${String(istDateTime.getUTCMonth() + 1).padStart(2, "0")}-${String(istDateTime.getUTCDate()).padStart(2, "0")}T${String(istDateTime.getUTCHours()).padStart(2, "0")}:${String(istDateTime.getUTCMinutes()).padStart(2, "0")}`;
   };
 
-  const createNestedTable = (data) => {
+  const createNestedTable = (data, modified = false, pre = data) => {
     if (typeof data !== 'undefined') {
       return Object.keys(data).map((key) => {
-        if (key === '0' || key === '1') return <tr><td></td>{createNestedTable(data[key])}</tr>
-        if (typeof data[key] === "object")
-          return createNestedTable(data[key]);
-        else return <dt id={key}>{data[key]}</dt>;
+        if (key === '0' || key === '1') return <tr className="inAudit"><td></td>{createNestedTable(data[key], key === '1', data["0"])}</tr>
+        if (typeof data[key] === "object") return createNestedTable(data[key], modified, pre[key] !== 'undefined' ? pre[key] : pre);
+        else return modified ? <dt className="inAudit" id={key}>{pre[key] === data[key] ? data[key] : <s style={{ textDecorationColor: "red" }}>{data[key]}</s>}</dt> : <dt id={key}>{data[key]}</dt>;
       });
     }
   };
@@ -38,39 +37,39 @@ const AuditLog = () => {
       <Loader />
     </dialog>
   ) : (
-    <div id="audit"><h1 style={{
+    <div><h1 style={{
       textAlign: "center"
     }}>Audit Log</h1>
-      <table>
+      <table className="inAudit">
 
-        <tr style={{
+        <tr className="inAudit" style={{
           borderBottom: 'solid'
-          }}>
+        }}>
+          <th id="c" className="inAudit">
             <input
-              className="fw-bolder bg-transparent text-black border-0 px-4"
-              type="text"
+              className="fw-bolder bg-transparent text-black border-0 px-4 inAudit"
+              type="button"
               value={"Modified Time"}
               disabled
-            />
-            <td></td>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Phone</th>
-          <th>City</th>
-          <th>State</th>
-          <th>PIN Code</th>
-          <th>Country</th>
+            /></th>
+          <th className="inAudit">ID</th>
+          <th className="inAudit">Name</th>
+          <th className="inAudit">Phone</th>
+          <th className="inAudit">City</th>
+          <th className="inAudit">State</th>
+          <th className="inAudit">PIN Code</th>
+          <th className="inAudit">Country</th>
         </tr>
 
       </table>
-      <table className="table w-100">
-        <tbody className="w-100">
+      <table className="table w-100 inAudit">
+        <tbody className="w-100 inAudit">
           {auditLog
             .slice()
             .reverse()
             .map((item) => (
               <tr
-                className={item.eventType === "EmployeeApi/Update" ? "update" : ""}
+                className={item.eventType === "EmployeeApi/Update" ? "update inAudit" : "inAudit"}
                 key={item.eventId}
                 style={{
                   "--bs-table-color": `var(--bs-${item.eventType === "POST Employees/Create" ||
@@ -90,12 +89,7 @@ const AuditLog = () => {
                 }}
               >
                 {/* <td>{item.eventId}</td>  */}
-                <input
-                  className="fw-bolder"
-                  type="datetime-local"
-                  value={convertUTCToIST(item.jsonData)}
-                  disabled
-                />
+
                 {/* <td>{item.user}</td> */}
                 {/* <td>
                 <details className="m-0 p-0">
@@ -103,8 +97,29 @@ const AuditLog = () => {
                   <ul>{createCollapsibleList(JSON.parse(item.jsonData), item.eventType)}</ul>
                 </details>
               </td> */}
-                {item.eventType !== "EmployeeApi/Update" ? <tr><td></td>{createNestedTable(JSON.parse(item.jsonData).Action.ActionParameters.employee)}</tr>:
-                  <>{createNestedTable(JSON.parse(item.jsonData).Action.ActionParameters.employee)}</>
+                {
+                  item.eventType !== "EmployeeApi/Update"
+                    ? <tr className="inAudit">
+                      <>
+                        <input type="button" className="inAudit" value={"Time"}></input>
+                        <input
+                          className="inAudit fw-bolder"
+                          type="datetime-local"
+                          value={convertUTCToIST(item.jsonData)}
+                          disabled
+                        />
+                      </><td></td>{createNestedTable(JSON.parse(item.jsonData).Action.ActionParameters.employee)}
+                    </tr>
+                    : <>
+                      <input type="button" className="inAudit" value={"Time"}></input>
+                      <input
+                        className="inAudit fw-bolder"
+                        type="datetime-local"
+                        value={convertUTCToIST(item.jsonData)}
+                        disabled
+                      />
+                      {createNestedTable(JSON.parse(item.jsonData).Action.ActionParameters.employee)}
+                    </>
                 }
               </tr>
             ))}
